276230fe79fc13ac7bd61f309a1d4c6554bde253
Alex Afanasiev <oafanasiev@css.edu>
2019-02-24 20:42:28 -0600

    Finished Participation Activity

--- app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
+++ app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
@@ -1,6 +1,8 @@
 package css.cis3334.intentsperformances;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.GregorianCalendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +24,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textViewStatus = (TextView) findViewById(R.id.textViewStatus);
-        setupButtonClickEvents();
+       // setupButtonClickEvents();
 
 
 
     }
+
+
+
+//Load the web page of this event
+    public void WebInfo1(View view)
+    {
+        Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d131356734%26view%3devent%26-childview%3d");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+        PackageManager packageManager = getPackageManager();
+        List activities = packageManager.queryIntentActivities(webIntent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        boolean isIntentSafe = activities.size() > 0;
+        startActivity(webIntent);
+    }
+
+//Load the web page of this event
+    public void WebInfo2(View view)
+    {
+        Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d131034185%26view%3devent%26-childview%3d");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+        PackageManager packageManager = getPackageManager();
+        List activities = packageManager.queryIntentActivities(webIntent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        boolean isIntentSafe = activities.size() > 0;
+        startActivity(webIntent);
+    }
+
+//Load the web page of this event
+    public void WebInfo3(View view)
+    {
+        Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d129951622%26view%3devent%26-childview%3d");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+        PackageManager packageManager = getPackageManager();
+        List activities = packageManager.queryIntentActivities(webIntent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        boolean isIntentSafe = activities.size() > 0;
+        startActivity(webIntent);
+    }
+
 
 
     private void setupButtonClickEvents() {
@@ -37,6 +79,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Do something in response to button click
                 textViewStatus.setText("Code should display website for performance 1");
+                Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures.html");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                PackageManager packageManager = getPackageManager();
+                List activities = packageManager.queryIntentActivities(webIntent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                boolean isIntentSafe = activities.size() > 0;
+                startActivity(webIntent);
             }
         });
 
@@ -64,6 +113,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+
         /**
          *   Set up button click event listener for the web info button for the first performance
          */
@@ -74,8 +126,79 @@ public class MainActivity extends AppCompatActivity {
                 textViewStatus.setText("Code should display map for performance 1");
             }
         });
+    }
+
+    public void addCalendar1(View view)
+    {
+
+        // Do something in response to button click
+
+        Intent calIntent = new Intent(Intent.ACTION_INSERT);
+        calIntent.setType("vnd.android.cursor.item/event");
+        calIntent.putExtra(CalendarContract.Events.TITLE, "SAL Colloquium 'The Robot Next Door'");
+        calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
+        GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
+        GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
+        calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                begDate.getTimeInMillis());
+        calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                endDate.getTimeInMillis());
+        startActivity(calIntent);
 
 
+    }
+
+    public void addCalendar2(View view)
+    {
+
+            // Do something in response to button click
+
+            Intent calIntent = new Intent(Intent.ACTION_INSERT);
+            calIntent.setType("vnd.android.cursor.item/event");
+            calIntent.putExtra(CalendarContract.Events.TITLE, "Health, Wholeness, and Humanity'");
+            calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "BWC");
+            GregorianCalendar begDate = new GregorianCalendar(2019, 2, 26, 7, 30);
+            GregorianCalendar endDate = new GregorianCalendar(2019, 2, 26, 8, 30);
+            calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                    begDate.getTimeInMillis());
+            calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                    endDate.getTimeInMillis());
+            startActivity(calIntent);
+
+
+    }
+
+    public void addCalendar3(View view)
+    {
+
+        // Do something in response to button click
+
+        Intent calIntent = new Intent(Intent.ACTION_INSERT);
+        calIntent.setType("vnd.android.cursor.item/event");
+        calIntent.putExtra(CalendarContract.Events.TITLE, "\n" +
+                "The Francis X. Shea Memorial Lecture Presents Poet Jane Hirshfield");
+        calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Mitchell Auditorium");
+        GregorianCalendar begDate = new GregorianCalendar(2019, 3, 5, 7, 30);
+        GregorianCalendar endDate = new GregorianCalendar(2019, 3, 5, 9, 30);
+        calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                begDate.getTimeInMillis());
+        calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                endDate.getTimeInMillis());
+        startActivity(calIntent);
+
+
+    }
+
+    public void map(View view)
+    {
+        // Map point based on address
+        Uri location = Uri.parse("https://www.google.com/maps/place/1200+Kenwood+Ave,+Duluth,+MN+55811/@46.8139092,-92.1170318,15.5z/data=!4m5!3m4!1s0x52afad45e28267a3:0x5c884be0749736a3!8m2!3d46.8163661!4d-92.1080999");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+        PackageManager packageManager = getPackageManager();
+        List activities = packageManager.queryIntentActivities(mapIntent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        boolean isIntentSafe = activities.size() > 0;
+        startActivity(mapIntent);
     }
 
 
